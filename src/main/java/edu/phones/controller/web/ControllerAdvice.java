07df05dda4b36ac597f9365dc4e430ef51a3d35e
Jlafratta <52461452+Jlafratta@users.noreply.github.com>
2020-05-12 18:26:32 -0300

    SpringBoot applied

---
+++ src/main/java/edu/phones/controller/web/ControllerAdvice.java
@@ -0,0 +1,35 @@
+package edu.phones.controller.web;
+
+import edu.phones.dto.ErrorResponseDto;
+import edu.phones.exceptions.InvalidLoginException;
+import edu.phones.exceptions.UserNotExistException;
+import edu.phones.exceptions.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(InvalidLoginException.class)
+    public ErrorResponseDto handleLoginException(InvalidLoginException exc) {
+        return new ErrorResponseDto(1, "Invalid login");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ValidationException.class)
+    public ErrorResponseDto handleValidationException(ValidationException exc) {
+        return new ErrorResponseDto(2, exc.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UserNotExistException.class)
+    public ErrorResponseDto handleUserNotExists() {
+        return new ErrorResponseDto(3, "User not exists");
+    }
+
+
+}
